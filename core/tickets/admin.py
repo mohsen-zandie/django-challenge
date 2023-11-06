ca6f9adeeccc2978adeee9ef38de9ba122904b9a
mohsen <mohsen.zandie1@gmail.com>
2023-11-06 14:16:07 +0330

    feat(tickets): add Ticket data model and TicketAdmin to admin interface
    Implemented the Ticket data model in the 'models.py' file and configured the TicketAdmin for the admin interface in the 'admin.py' file of the 'tickets' app. These additions enable the management of ticket information and related functionalities within the application's admin panel.

--- core/tickets/admin.py
+++ core/tickets/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from tickets.models import Ticket
+
 # Register your models here.
+
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('event', 'user', 'seat', 'price',
+                    'created_at', 'updated_at')
+    list_filter = ('event', 'user', 'created_at', 'updated_at')
+    search_fields = ('event', 'user', 'seat', 'price',
+                     'created_at', 'updated_at')
+
+
+admin.site.register(Ticket, TicketAdmin)
--- core/tickets/models.py
+++ core/tickets/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from events.models import Event
 
-# Create your models here.
+# getting user model
+User = get_user_model()
+
+
+class Ticket(models.Model):
+    '''
+    Ticket model
+    '''
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    seat = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.event} - {self.user}'
