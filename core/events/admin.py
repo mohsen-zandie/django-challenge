5ad4f57d8302e48f6b697e751a5d5e1eb9876056
mohsen <mohsen.zandie1@gmail.com>
2023-11-06 14:14:01 +0330

    feat(events): customize admin interface for Stadium and Event models
    Configured the admin interface for the Stadium and Event models in the 'admin.py' file of the 'events' app. The customizations include defining specific fields for display, enabling filtering and search capabilities, and enhancing the overall user experience for managing stadium and event data within the application.

--- core/events/admin.py
+++ core/events/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from events.models import Stadium, Event
 
-# Register your models here.
+
+class StadiumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'capacity', 'city', 'country',
+                    'address', 'opened', 'created_at', 'updated_at')
+    list_filter = ('name', 'capacity', 'city', 'country',
+                   'address', 'opened', 'created_at', 'updated_at')
+    search_fields = ('name', 'capacity', 'city', 'country',
+                     'address', 'opened', 'created_at', 'updated_at')
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stadium', 'date', 'description',
+                    'price', 'created_at', 'updated_at')
+    list_filter = ('name', 'stadium', 'date', 'description',
+                   'price', 'created_at', 'updated_at')
+    search_fields = ('name', 'stadium', 'date', 'description',
+                     'price', 'created_at', 'updated_at')
+
+
+admin.site.register(Stadium, StadiumAdmin)
+admin.site.register(Event, EventAdmin)
