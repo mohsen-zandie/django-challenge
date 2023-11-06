e976a945bc9fae1750989c6aa881fec2d917ac41
mohsen <mohsen.zandie1@gmail.com>
2023-11-06 12:11:23 +0330

    feat(accounts): create Profile after User creation using signals
    Added code to the 'models.py' file in the 'accounts' app to create a Profile instance automatically when a User is created, using Django signals. This ensures the creation of a corresponding profile for every new user in the application.

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserManager(BaseUserManager):
@@ -67,3 +69,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.email
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
