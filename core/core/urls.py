c09bf50dfffedffbe3746bcd54166c4341662ba6
mohsen <mohsen.zandie1@gmail.com>
2023-11-06 23:49:27 +0330

    feat(core): integrate Swagger for API documentation
    Integrated Swagger into the project to enable interactive API documentation. Updated the 'requirements.txt' file to include the necessary dependencies, made relevant configurations in the 'settings.py' file, and added the required URL mappings in the 'urls.py' file. This enhancement facilitates better understanding and testing of the project's API endpoints.

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,12 +19,44 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.urls.conf import include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from core import settings
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Django Challenge Api",
+        default_version="v1",
+        description="this is a test api for cinema ticket django challenge project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="mohsen.zandie1@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api_auth/', include('rest_framework.urls')),
     path('events/', include('events.urls')),
+    path(
+        "swagger/output.json",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
 
 # Serve static and media files in development
