6fc9c9a50d71e5b0fd64cc78efe614f12459709f
mohsen <mohsen.zandie1@gmail.com>
2023-11-07 00:04:03 +0330

    feat(accounts): add api/v1/ package with URLs and views
    Created the 'api/v1/' package along with the necessary 'urls.py' and 'views.py' files to facilitate the organization and implementation of version 1 of the API. This setup lays the foundation for handling API requests and responses within the project.

---
+++ core/accounts/api/v1/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+
+app_name = 'api-v1'
+
+# router = DefaultRouter()
+# router.register("account", views.EventModelViewSet, basename="account")
+
+# urlpatterns = router.urls
+
+urlpatterns = []
--- core/core/urls.py
+++ core/core/urls.py
@@ -41,6 +41,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api_auth/', include('rest_framework.urls')),
+    path("accounts/", include("accounts.urls")),
     path('events/', include('events.urls')),
     path(
         "swagger/output.json",
