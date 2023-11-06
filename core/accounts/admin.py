93df2e8dd18a086a2727503a524c654101d199af
mohsen <mohsen.zandie1@gmail.com>
2023-11-06 11:55:35 +0330

    feat(accounts): add Profile data model and register to admin interface
    Implemented the Profile data model in the 'models.py' file of the 'accounts' app. Additionally, registered the Profile model to the Django admin interface in the 'admin.py' file. This allows for the management of user profiles and associated information within the application.

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from accounts.models import User
+from accounts.models import Profile
 
 
 class CustomUserAdmin(UserAdmin):
@@ -23,3 +24,4 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -51,3 +51,19 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class Profile(models.Model):
+    '''
+    User profile model 
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=255, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
