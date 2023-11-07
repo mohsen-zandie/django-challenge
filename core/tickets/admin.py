a02307a6774548ba31bf0ed1210fa5a473b66ab1
mohsen <mohsen.zandie1@gmail.com>
2023-11-07 21:22:42 +0330

    feat(ticket): enable adding multiple tickets to a single basket
    Enhanced the functionality within the 'ticket' app to enable the addition of multiple tickets to a single basket. Leveraged the Many-to-Many relationship between the 'Basket' and 'Ticket' models, providing users with the flexibility to add multiple tickets to their basket during the ticket purchase process.

--- core/tickets/admin.py
+++ core/tickets/admin.py
@@ -6,19 +6,26 @@ from tickets.models.baskets import Basket
 # Register your models here.
 
 
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 0
+
+
 class TicketAdmin(admin.ModelAdmin):
     list_display = (
         "event",
         "user",
+        "basket",
         "seat",
         "price",
         "created_at",
         "updated_at",
     )
-    list_filter = ("event", "user", "created_at", "updated_at")
+    list_filter = ("event", "user", "basket", "created_at", "updated_at")
     search_fields = (
         "event",
         "user",
+        "basket",
         "seat",
         "price",
         "created_at",
@@ -29,19 +36,28 @@ class TicketAdmin(admin.ModelAdmin):
 class BasketAdmin(admin.ModelAdmin):
     list_display = (
         "user",
-        "ticket",
         "is_paid",
         "created_at",
         "updated_at",
     )
-    list_filter = ("user", "ticket", "is_paid", "created_at", "updated_at")
+    list_filter = ("user", "is_paid", "created_at", "updated_at")
     search_fields = (
         "user",
-        "ticket",
         "is_paid",
         "created_at",
         "updated_at",
     )
+    inlines = [TicketInline]
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super().get_inline_instances(request, obj)
+
+    def tickets(self, obj):
+        return "\n".join([ticket.__str__() for ticket in obj.ticket_set.all()])
+
+    tickets.short_description = 'Tickets'
 
 
 admin.site.register(Ticket, TicketAdmin)
--- core/tickets/models/tickets.py
+++ core/tickets/models/tickets.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from events.models import Event
+from tickets.models.baskets import Basket
 
 # getting user model
 User = get_user_model()
@@ -14,6 +17,8 @@ class Ticket(models.Model):
 
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    basket = models.ForeignKey(
+        Basket, on_delete=models.CASCADE, null=True, blank=True)
     seat = models.CharField(max_length=10)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -21,3 +26,14 @@ class Ticket(models.Model):
 
     def __str__(self):
         return f"{self.event} - {self.user}"
+
+
+@receiver(post_save, sender=Ticket)
+def create_basket_for_ticket(sender, instance, created, **kwargs):
+    if created and not instance.basket:
+        try:
+            basket = Basket.objects.get(user=instance.user)
+        except Basket.DoesNotExist:
+            basket = Basket.objects.create(user=instance.user)
+        instance.basket = basket
+        instance.save()
